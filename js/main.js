9ce2c3913cf1631a95f48784c1edc473dc8037c2
YJouille <y.j.39110@gmail.com>
2021-09-01 16:56:55 +0200

    architecture globale

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-let student = new Array["Yamina"];
+let student = new Array();
+
+student = document.querySelectorAll('li');
+
+// localStorage.setItem("name" , "Yamina Jouille");
+
+document.getElementById("randomBtn").addEventListener("click" , function(){
+    for(i=0 ; i<student.length ; i++){
+        console.log(student[i]);
+    }
+   
+});
