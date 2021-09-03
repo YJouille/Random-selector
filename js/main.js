b1d80e9159eeac15f069662527b48aba8ed4019d
YJouille <y.j.39110@gmail.com>
2021-09-03 16:41:03 +0200

    commit de fin de journée

--- js/main.js
+++ js/main.js
@@ -1,58 +1,74 @@
 let notSelectedStudent = new Array();
-notSelectedStudent = ["Dylan","Alphonso","Yamina","Arthur","Laurent","Célèste"];
+notSelectedStudent = [
+  "Dylan",
+  "Alfonso",
+  "Yamina",
+  "Arthur",
+  "Laurent",
+  "Célèste",
+  "Francis",
+  "Marley",
+  "Lucas",
+  "Aleaddine",
+  "Tommy",
+  "Josélito",
+];
 let selectedStudent = new Array();
+let randomIndex = new Number();
 
-let randomIndex = new Number;
 
 
 //transformer le tableau en chaine de caractères et le stocker en local
-localStorage.setItem("apprenantNoSelected",notSelectedStudent.join(", "));
+localStorage.setItem("apprenantNoSelected", notSelectedStudent.join(", "));
 
-
-// Affiche la liste des non selectionnés à partir du stackage local
+// Au chargement de la page affiche la liste des non selectionnés à partir du stackage local
 notSelectedStudentLocal = localStorage.getItem("apprenantNoSelected").split(", ");
 
 for (let i = 0; i < notSelectedStudentLocal.length; i++) {
-    document.getElementById("noSelected").innerHTML += notSelectedStudentLocal[i]+'<br>';   
+  document.getElementById("noSelected").innerHTML +=
+    notSelectedStudentLocal[i] + "<br>";
 }
+let count = notSelectedStudentLocal.length;
 
-
-document.getElementById("randomBtn").addEventListener("click" , function(){  
-   
-
-     //tant qu'il ya des élements dans les non selectionnés
-    if(notSelectedStudentLocal.length !=0){
-        //Generate a random index
-
-    randomIndex = Math.floor(Math.random() * notSelectedStudentLocal.length);
-    //affiche le random index
-    console.log(randomIndex);
- 
-    //affiche l'élément selectionné
-    console.log(notSelectedStudentLocal[randomIndex]);
+//au click sur le bouton
+document.getElementById("randomBtn").addEventListener("click", function () {
+  //tant qu'il ya des élements dans la liste locale des non séléctionnés
   
+  if (count != 0) {
+    //Generate a random index
+    randomIndex = Math.floor(Math.random() * notSelectedStudentLocal.length);
+
     //ajouter l'element dans les selectionnés
     selectedStudent.push(notSelectedStudentLocal[randomIndex]);
 
-    //affiche cet élement
-    document.getElementById("selected").innerHTML += notSelectedStudentLocal[randomIndex]+'<br>';
+    //affiche cet élement dans la colonne de droite
+    document.getElementById("selected").innerHTML += notSelectedStudentLocal[randomIndex] + "<br>";
 
     //supprimer l'élément dans le tableau des non selectionné
     notSelectedStudentLocal.splice(randomIndex, 1);
 
-    document.getElementById("noSelected").innerHTML = notSelectedStudentLocal+'<br>';
+    //enregistrer en local la nouvelle liste des non selectionnes
+    localStorage.setItem("apprenantNoSelected", notSelectedStudentLocal.join(", "));
 
-    //supprimer l'élément du stockage local
 
-    //localStorage.setItem("apprenantNoSelected",notSelectedStudentLocal.join(", "));
+    // Affiche la nouvelle liste des non selectionnés à partir du stackage local
 
-    console.log(notSelectedStudentLocal)
+    notSelectedStudentLocal = localStorage.getItem("apprenantNoSelected").split(", ");
+    //effacer le contenu de la colonne de gauche
+    document.getElementById("noSelected").innerHTML = "";
+
+    for (let i = 0; i < notSelectedStudentLocal.length; i++) {
+      document.getElementById("noSelected").innerHTML +=
+        notSelectedStudentLocal[i] + "<br>";
+    }
+    
+
     //transformer le tableau en chaine de caractères et le stocker localement
-    localStorage.setItem("apprenantSelected",selectedStudent.join(", "));
+    localStorage.setItem("apprenantSelected", selectedStudent.join(", "));
+  } else {
+    alert("tout le monde sélectionné!");
 
-    }else{
-        console.log('tout le monde selectionné');
-        console.log('apprenant selectionnés '+selectedStudent);   
-    } 
-
+    // console.log("apprenant selectionnés " + selectedStudent);
+  }
+  count -=1;
 });
