18565920ff0ca5ffb6f137b1154b806b576dc61c
YJouille <y.j.39110@gmail.com>
2021-09-07 16:47:47 +0200

    fin style

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 let notSelectedStudents = new Array();
-notSelectedStudents = ["Dylan", "Alfonso", "Yamina", "Arthur", "Laurent", "Célèste", "Francis", "Marley", "Lucas", "Aleaddine", "Tommy", "Josélito"];
+notSelectedStudents = [
+  "Dylan",
+  "Alfonso",
+  "Yamina",
+  "Arthur",
+  "Laurent",
+  "Célèste",
+  "Francis",
+  "Marley",
+  "Lucas",
+  "Aleaddine",
+  "Tommy",
+  "Josélito",
+];
 let notSelectedStudentsLocal = new Array();
 let selectedStudentsLocal = new Array();
 let randomIndex = new Number();
 let nbvisites;
-
 
 /*******Définition des fonction*******/
 // Cette fonction indique si c'est un premier chargement ou une actualisation
@@ -32,12 +44,11 @@ function visitCounter() {
 }
 
 // Cette fonction affiche le resultat en html
-function display(idDiv , arrayStudents){
-  document.getElementById(idDiv).innerHTML = '';
-    for (let i = 0; i < arrayStudents.length; i++) {
-      document.getElementById(idDiv).innerHTML +=
-      arrayStudents[i] + "<br>";
-    }
+function display(idDiv, arrayStudents) {
+  document.getElementById(idDiv).innerHTML = "";
+  for (let i = 0; i < arrayStudents.length; i++) {
+    document.getElementById(idDiv).innerHTML += arrayStudents[i] + "<br>";
+  }
 }
 
 /*******Appel des fonctions*******/
@@ -45,82 +56,82 @@ nbvisites = visitCounter();
 
 //cahrgement
 window.addEventListener("load", function () {
-
   console.log("nombre visites " + nbvisites);
 
   if (nbvisites == 2) {
     //au premier chargement : la liste de gauche complète et celle de droite vide
     //liste de gauche
-    display("noSelected" , notSelectedStudents );
+    display("noSelected", notSelectedStudents);
 
     //ecrire cette liste complète en local en transformant le tableau en chaine de caractères et le stocker en local
-     localStorage.setItem("apprenantNoSelected", notSelectedStudents.join(","));
-     notSelectedStudentsLocal = localStorage.getItem("apprenantNoSelected").split(",");
+    localStorage.setItem("apprenantNoSelected", notSelectedStudents.join(","));
+    notSelectedStudentsLocal = localStorage
+      .getItem("apprenantNoSelected")
+      .split(",");
   } else {
     //dans le cas d'une actualisation
-   //liste de gauche : récuperer et afficher le contenu de la liste locale des non selected
-   notSelectedStudentsLocal = localStorage.getItem("apprenantNoSelected").split(",");
+    //liste de gauche : récuperer et afficher le contenu de la liste locale des non selected
+    notSelectedStudentsLocal = localStorage
+      .getItem("apprenantNoSelected")
+      .split(",");
 
-   display("noSelected" , notSelectedStudentsLocal );
+    display("noSelected", notSelectedStudentsLocal);
 
-   //liste de droite : récuperer et afficher le contenu de la liste locale des selected
-   console.log('selectedStudentsLocal'+selectedStudentsLocal);
+    //liste de droite : récuperer et afficher le contenu de la liste locale des selected
 
-   if (localStorage.getItem('apprenantSelected')){
-    selectedStudentsLocal = localStorage.getItem("apprenantSelected").split(",");
-   }
-
-
-
-   display("selected" , selectedStudentsLocal );
+    if (localStorage.getItem("apprenantSelected")) {
+      selectedStudentsLocal = localStorage
+        .getItem("apprenantSelected")
+        .split(",");
+    }
+    display("selected", selectedStudentsLocal);
   }
 });
-
-
 
 //au click sur le bouton
 document.getElementById("randomBtn").addEventListener("click", function () {
   let count = notSelectedStudentsLocal.length;
-  console.log('count '+count);
-  
   if (count > 0) {
-    
-  //Generate a random index
-  randomIndex = Math.floor(Math.random() * count);
+    //Generate a random index
+    randomIndex = Math.floor(Math.random() * count);
 
-   //ajouter l'element dans les selectionnés
-   selectedStudentsLocal.push(notSelectedStudentsLocal[randomIndex]);
+    //ajouter l'element dans les selectionnés
+    selectedStudentsLocal.push(notSelectedStudentsLocal[randomIndex]);
+
+    //Affiche dans la div resultat
+    document.getElementById("result").innerHTML =
+    notSelectedStudentsLocal[randomIndex];
 
     //colonne de droite
     //affiche cet élement dans la colonne de droite
-    document.getElementById("selected").innerHTML += notSelectedStudentsLocal[randomIndex] + "<br>";
-   
+    document.getElementById("selected").innerHTML +=
+      notSelectedStudentsLocal[randomIndex] + "<br>";
 
-   //supprimer l'élément dans le tableau des non selectionné local
-   notSelectedStudentsLocal.splice(randomIndex, 1);
+    //supprimer l'élément dans le tableau des non selectionné local
+    notSelectedStudentsLocal.splice(randomIndex, 1);
 
     //enregistrer en local la nouvelle liste des non selectionnes
-    localStorage.setItem("apprenantNoSelected", notSelectedStudentsLocal.join(","));
-
+    localStorage.setItem(
+      "apprenantNoSelected",
+      notSelectedStudentsLocal.join(",")
+    );
     //transformer le tableau en chaine de caractères et le stocker localement
-    console.log ('selectedStudentsLocal '+selectedStudentsLocal);
-
     localStorage.setItem("apprenantSelected", selectedStudentsLocal.join(","));
 
-    display("noSelected" , notSelectedStudentsLocal );
-   
+    display("noSelected", notSelectedStudentsLocal);
+
     count -= 1;
   } else {
-    selectedStudentsLocal =[];
-    alert("tout le monde sélectionné!");
-    display("noSelected" , notSelectedStudents ); //liste initiale complète à gauche
-    localStorage.setItem("apprenantNoSelected", notSelectedStudents.join(","));
+    //Clear la zone de resultat
+    document.getElementById("result").innerHTML ='';
 
+    selectedStudentsLocal = [];
+    display("noSelected", notSelectedStudents); //liste initiale complète à gauche
+    localStorage.setItem("apprenantNoSelected", notSelectedStudents.join(","));
     document.getElementById("selected").innerHTML = "";
 
     localStorage.removeItem("apprenantSelected");
     //vider le tableau des apprenants selected
-
     notSelectedStudentsLocal = localStorage.getItem("apprenantNoSelected").split(",");
-}
+  }
 });
